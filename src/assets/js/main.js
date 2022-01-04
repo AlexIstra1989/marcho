646a293f9b591cd2c5a76d4b18e80565ac6406ca
AlexIstra1989 <alex888fcsm@gmail.com>
2022-01-04 23:59:01 +0300

    added filters

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -7,4 +7,21 @@ $(document).ready(function() {
   slidesToShow: 1,
   slidesToScroll: 1,
  });
+
+
+$('.filter-price__field').ionRangeSlider({
+ type: "double",
+ min: 0,
+ max: 999,
+ prefix: $,
+ onStart: function (data) {
+  $('.filter-price__from').text(data.from);
+  $('.filter-price__to').text(data.to);
+ },
+ onChange: function (data) {
+  $('.filter-price__from').text(data.from);
+  $('.filter-price__to').text(data.to);
+ },
+});
+
 });
